0ee814c9e303cf94dba281afdf72f3562c4b57e9
deepbuzin <contactbuzin@gmail.com>
2024-09-23 20:13:52 +0700

    Initial Commit

---
+++ Robot/RobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotApp.swift
+//  Robot
+//
+//  Created by Andrey Buzin on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct RobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RobotTests/RobotTests.swift
@@ -0,0 +1,17 @@
+//
+//  RobotTests.swift
+//  RobotTests
+//
+//  Created by Andrey Buzin on 23.09.2024.
+//
+
+import Testing
+@testable import Robot
+
+struct RobotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
