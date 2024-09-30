bfbf86eccc9d67f136241ba0295e0d5bd8ec6d9d
deepbuzin <contactbuzin@gmail.com>
2024-09-30 22:58:32 +0700

    Implement green bounding box for video for upright orientation

--- Robot/FaceTrackingView.swift
+++ Robot/FaceTrackingView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 import Vision
 import AVFoundation
 
-class TrackingManager: NSObject, ObservableObject, AVCaptureVideoDataOutputSampleBufferDelegate {
-    @Published var session: AVCaptureSession?
-    @Published var isAuthorized: Bool = false
+@Observable class TrackingManager: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+    var session: AVCaptureSession?
+    var isAuthorized: Bool = false
     
     let detectRequest = DetectFaceRectanglesRequest()
-    @State var detectionStatus: String = "nothing yet"
-    @State var observations: [FaceObservation] = []
-    @State var image: UIImage?
+    
+    var detectionStatus: String = "nothing yet"
+    var observations: [FaceObservation] = []
+    var image: UIImage?
     
     func checkAuthorization() async -> Bool {
         let status = AVCaptureDevice.authorizationStatus(for: .video)
@@ -31,7 +32,7 @@ class TrackingManager: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
         do {
             let session = AVCaptureSession()
             
-            guard let device = AVCaptureDevice.default(.builtInTrueDepthCamera, for: .video, position: .front) else {
+            guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
                 print("Failed to get the front camera device")
                 return
             }
@@ -63,13 +64,13 @@ class TrackingManager: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
     
     func performDetection(sampleBuffer: CMSampleBuffer) async {
         do {
-            let response = try await detectRequest.perform(on: sampleBuffer)
+            let response = try await detectRequest.perform(on: sampleBuffer, orientation: .left)
             
             await MainActor.run {
                 updateObservations(response)
-                print(response)
+//                print(UIDevice.current.orientation.rawValue)
+//                print(response)
             }
-            
             
         } catch {
             detectionStatus = "caught error: \(error.localizedDescription)"
@@ -83,6 +84,25 @@ class TrackingManager: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
     }
 }
 
+struct FaceOverlayView: View {
+    var observations: [FaceObservation]
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ForEach(observations, id: \.self) { observation in
+                Path { path in
+                    let rect = observation.boundingBox.toImageCoordinates(geometry.size, origin: .lowerLeft)
+                    path.addRect(rect)
+                }
+                .stroke(.green, lineWidth: 2)
+            }
+        }
+//        .onChange(of: observations) { _, _ in
+//            print("observations changed")
+//        }
+    }
+}
+
 struct TrackingPreview: UIViewRepresentable {
     let session: AVCaptureSession
         
@@ -91,23 +111,37 @@ struct TrackingPreview: UIViewRepresentable {
         let previewLayer = AVCaptureVideoPreviewLayer(session: session)
         previewLayer.frame = view.bounds
         previewLayer.videoGravity = .resizeAspectFill
+        
+//        previewLayer.connection?.videoRotationAngle = .zero
+        
         view.layer.addSublayer(previewLayer)
         return view
     }
     
     func updateUIView(_ uiView: UIView, context: Context) {
-        
+//        context.coordinator
     }
+    
+//    func makeCoordinator() -> () {
+//        return Coordinator()
+//    }
+//    
+//    class Coordinator {
+//        var previewLayer: AVCaptureVideoPreviewLayer
+//    }
 }
 
 
 struct FaceTrackingView: View {
-    @StateObject var trackingManager = TrackingManager()
+    @State var trackingManager = TrackingManager()
     
     var body: some View {
         ZStack {
             if let session = trackingManager.session {
                 TrackingPreview(session: session)
+                    .overlay {
+                        FaceOverlayView(observations: trackingManager.observations)
+                    }
             } else {
                 Text("No camera for you").foregroundColor(.white)
             }
@@ -120,27 +154,30 @@ struct FaceTrackingView: View {
                }
                            
             }
-        }.onAppear {
-            Task {
-                let isAuthorized = await trackingManager.checkAuthorization()
-                await MainActor.run {
-                    trackingManager.isAuthorized = isAuthorized
-                }
-                
-                if trackingManager.isAuthorized {
-                    trackingManager.setupCamera()
-                    
-                    await MainActor.run {
-                        if let session = trackingManager.session {
-                            let output = AVCaptureVideoDataOutput()
-                            if session.canAddOutput(output) {
-                                session.addOutput(output)
-                            }
-                            
-                            let queue = DispatchQueue(label: "videoQueue")
-                            output.setSampleBufferDelegate(trackingManager, queue: queue)
-                        }
+        }.task {
+            await setupCamera()
+        }
+    }
+    
+    func setupCamera() async {
+        let isAuthorized = await trackingManager.checkAuthorization()
+        await MainActor.run {
+            trackingManager.isAuthorized = isAuthorized
+        }
+        
+        if trackingManager.isAuthorized {
+            trackingManager.setupCamera()
+            
+            await MainActor.run {
+                if let session = trackingManager.session {
+                    let output = AVCaptureVideoDataOutput()
+                    if session.canAddOutput(output) {
+                        session.addOutput(output)
                     }
+                    
+                    let queue = DispatchQueue(label: "videoQueue")
+                    output.setSampleBufferDelegate(trackingManager, queue: queue)
+//                    output.connection(with: .video)?.videoRotationAngle = .zero
                 }
             }
         }
